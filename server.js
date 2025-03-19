58237a403ad0e8e0404ff9f71d903e14dbbbbf61
Ahri9tails <ahri9tails@proton.me>
2025-03-19 09:48:28 +0900

    store users in user object, and add room scope to emits.

--- server.js
+++ server.js
@@ -17,38 +17,114 @@ const io = socketio(expressServer,{
 
 })
 
-
+//maybe change to user list or user container or user database or user 
 const users = {}
+let currentUser = ""
+
+//key of this object to be the id of this socket
+
+/* function userJoin(id, username, room) {
+	const user = { id, username, room }
+
+	users.push(user)
+
+	return user
+} */
+
+/* function getCurrentUser(id) {
+	return users.find(user => user.id === id)
+} */
+
+
+
+//users[socket.id]
+//I want this to grab an object with keys: name, room, id maybe
+
+//currentUser = users[socket.id]
+// currentUser.username or currentUser.room
+
+//socket.id must become an object with name, room, and id
+
+// can use object destructuring to get the key values
+// define a set of variables with the key names, and each
+// of those variables will be assigned the key value
+// then you can use the variables.
+
+
+/* const users = {
+	afhrherafjagasdfd:"username",
+	nextsocketid:"nextusernameid"
+} */
+
 
 // on is a regular js/node event listener
 // emit is another method
 io.on("connect",socket=>{
+	//sends the number of users
 	socket.emit("join", Object.keys(users).length)
-	socket.on("new-user", username => {
-		users[socket.id] = username
-		console.log("all the user objects", users)
-		updateUserlist()
+	socket.on("new-user", ({ username, room }) => {
+		const user = createUserObject(socket.id, username, room)
+		users[socket.id] = user
+
+		socket.join(user["room"]) 
+		// need to send username and room on "new-user" emit
+		// add username to the socket id in the user list
+		// add socket.room
+		console.log("username", username)
+		console.log("room", room)
+		currentUser = users[socket.id]
+		console.log("test get user", currentUser["room"])
+
+
+		console.log("all the user objects", users, room)
+		console.log("current socket user object", users[socket.id])
+		updateUserlist(currentUser)
 	})
 	console.log(socket.id, " has joined the server.")
 	//emit("event name", data)
 
 	socket.on("roll", rollResultString=>{
 		console.log(rollResultString)
-		socket.broadcast.emit("roll-result", rollResultString)
+		currentUser = users[socket.id]
+		//
+		socket.broadcast.to(currentUser["room"]).emit("roll-result", rollResultString)
 	})
 
 	socket.on("change-username", username => {
-		users[socket.id] = username
-		updateUserlist()
+		currentUser["username"] = username
+		updateUserlist(currentUser)
 	})
 
 	socket.on("disconnect", () => {
 		delete users[socket.id]
-		updateUserlist()
+		updateUserlist(currentUser)
 	})
 })
 
-function updateUserlist() {
+function updateUserlist(currentUser) {
+	//this is going to print all the values of the users
+	//it probably will not work once rooms are added
 	userlist = Object.values(users)
-	io.emit("user-connected", userlist)
+	console.log(".values of users", Object.values(users))
+	io.to(currentUser["room"]).emit("user-connected", userlist)
 }
+
+function createUserObject(id, username, room ) {
+	//socket.id, string, string
+	const user = {
+		id: id,
+		username: username,
+		room: room
+	}
+
+	return user
+}
+
+
+
+// find all users inside of users with the user.id that equals the current id
+// we can probably use this to find all users inside of users with the user.room that equals the current room
+// this would return multiple results, probably a list of objects?
+
+//get something
+//return users.find(user => user.id === id) 
